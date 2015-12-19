19568f1de54b847266eaffbe7ea49f111907ca3a
qio <info.qio.io@gmail.com>
2015-12-18 22:08:12 -0400

    Changing name to uuid

--- src/uuid.cr
+++ src/uuid.cr
@@ -2,22 +2,20 @@ require "random"
 require "./uuid/*"
 
 module UUID
-
   def self.generate
-		Random.new_seed
-		"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".gsub(/[xy]/) { |s|
-			r = (Random.rand*16).to_i|0
-			if s == "x"
-				v = r
-			elsif
-				v = (r & 0x3 | 0x8)
-			end
-			v.to_s(16)
-		}
+    Random.new_seed
+    "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".gsub(/[xy]/) { |s|
+      r = (Random.rand*16).to_i|0
+      if s == "x"
+        v = r
+      elsif
+        v = (r & 0x3 | 0x8)
+      end
+      v.to_s(16)
+    }
   end
 
-	def self.generate_simple
-		self.generate.gsub("-", "")
-	end
-
+  def self.generate_simple
+    self.generate.gsub("-", "")
+  end
 end
